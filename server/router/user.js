2ab5b708b99aa9b08c1192652702146e26fc5671
liwenxiong <13724794536@163.com>
2022-03-23 01:23:37 +0800

    feat: 添加eslint检查

--- server/router/user.js
+++ server/router/user.js
@@ -1,7 +1,6 @@
 const Router = require('koa-router')
 const { create } = require('../controller/user')
 const { verifyUser } = require('../middleware/user')
-const { ParameterException } = require('../utils/http-exception')
 
 const userRouter = new Router({ prefix: '/user' })
 
